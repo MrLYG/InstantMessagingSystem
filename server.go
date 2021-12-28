59aa45de1e09a8468ba2a1d0a95e03081be33381
yuangang <liyuangang@sensetime.com>
2021-12-28 18:50:41 +0800

    v0.2 用户上线广播功能

--- server.go
+++ server.go
@@ -3,34 +3,83 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	//在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//消息广播Channel
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
+//监听Message广播消息channel的goroutine，一旦有消息就发送给全部在线User
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+
+		//将msg发送给全部的在线User
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+//广播消息方法
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
 func (this *Server) Handler(conn net.Conn) {
 	//当前connect的业务
-	fmt.Println("connect 建立成功")
+	fmt.Println("connect建立成功")
+	user := NewUser(conn)
+	//用户上线
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	this.BroadCast(user, "已上线")
+
+	//当前handler阻塞
+	select {}
 }
 
 //启动服务器端口
 func (this *Server) Start() {
+
 	//socket listen
 	listner, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
 		fmt.Println("net.Listen err:", err)
 		return
 	}
+	//close listen socket
+	defer listner.Close()
+
+	//启动监听Message的goroutine
+	go this.ListenMessager()
+
 	for {
 		//accept
 		conn, err := listner.Accept()
@@ -42,6 +91,4 @@ func (this *Server) Start() {
 		go this.Handler(conn)
 	}
 
-	//close listen socket
-	defer listner.Close()
 }
